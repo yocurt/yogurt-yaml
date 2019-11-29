d727829b953c1cc3f2e0ba30d68a1627bee91d1d
Johannes Jeising <Johannes.Jeising@gmail.com>
2019-11-29 22:01:36 +0100

    improve benchmark tests

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -9,7 +9,7 @@ use libcurt::YogurtYaml;
 fn criterion_benchmark(c: &mut Criterion) {
     let mut curt = YogurtYaml::new(&["ID", "ADD", "REF", "END"]);
     let test_data = "other stuff ID[Test, \nTestContent: \"3\"] more\n REF[Test2, \nTestContent: [4]\n] stuADD[Test3, TestContent: [[a,7],[a,d]]]".repeat(5);
-    c.bench_function("Yogurt Yaml Curt", |b| {
+    c.bench_function("YogurtYaml.curt()", |b| {
         b.iter(|| curt.curt(black_box(&test_data)))
     });
 }
