026101a4cb11857389b8e8c91dcb71bd982ba3bc
Johannes Jeising <Johannes.Jeising@gmail.com>
2019-11-29 18:56:51 +0100

    Add benchmark test via criterion

---
+++ benches/benchmark.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+use criterion::black_box;
+
+use libcurt::YogurtYaml;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut curt = YogurtYaml::new(&["ID", "ADD", "REF", "END"]);
+    let test_data = "other stuff ID[Test, \nTestContent: \"3\"] more\n REF[Test2, \nTestContent: [4]\n] stuADD[Test3, TestContent: [[a,7],[a,d]]]".repeat(5);
+    c.bench_function("Yogurt Yaml Curt", |b| b.iter(|| curt.curt(black_box(&test_data))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
