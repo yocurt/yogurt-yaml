38c23e8fff296d9e73984d63ef30b62c77b45c99
Johannes Jeising <Johannes.Jeising@gmail.com>
2019-12-07 21:11:12 +0100

    update benchmark test

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -4,10 +4,21 @@ extern crate criterion;
 use criterion::black_box;
 use criterion::Criterion;
 
-use libcurt::YogurtYaml;
+use libcurt::{IdentRange, Indicators, YogurtYaml};
+
+fn criterion_benchmark_curt_multi(c: &mut Criterion) {
+    let mut indicators = Vec::new();
+    let indicator = Indicators::new(&["ID", "ADD", "REF", "END"], IdentRange::Bracket);
+    indicator.push(indicator);
+    let mut curt = YogurtYaml::new(indicators);
+    let test_data = "other stuff ID[Test, \nTestContent: \"3\"] more\n REF[Test2, \nTestContent: [4]\n] stuADD[Test3, TestContent: [[a,7],[a,d]]]".repeat(5);
+    c.bench_function("YogurtYaml.curt()", |b| {
+        b.iter(|| curt.curt(black_box(&test_data)))
+    });
+}
 
 fn criterion_benchmark_curt_5(c: &mut Criterion) {
-    let mut curt = YogurtYaml::new(&["ID", "ADD", "REF", "END"]);
+    let mut curt = YogurtYaml::new_from_str(&["ID", "ADD", "REF", "END"]);
     let test_data = "other stuff ID[Test, \nTestContent: \"3\"] more\n REF[Test2, \nTestContent: [4]\n] stuADD[Test3, TestContent: [[a,7],[a,d]]]".repeat(5);
     c.bench_function("YogurtYaml.curt()", |b| {
         b.iter(|| curt.curt(black_box(&test_data)))
@@ -15,7 +26,7 @@ fn criterion_benchmark_curt_5(c: &mut Criterion) {
 }
 
 fn criterion_benchmark_curt_1(c: &mut Criterion) {
-    let mut curt = YogurtYaml::new(&["ID", "ADD", "REF", "END"]);
+    let mut curt = YogurtYaml::new_from_str(&["ID", "ADD", "REF", "END"]);
     let test_data = "other stuff ID[Test, \nTestContent: \"3\"] more\n REF[Test2, \nTestContent: [4]\n] stuADD[Test3, TestContent: [[a,7],[a,d]]]".repeat(1);
     c.bench_function("YogurtYaml.curt(short)", |b| {
         b.iter(|| curt.curt(black_box(&test_data)))
@@ -25,6 +36,7 @@ fn criterion_benchmark_curt_1(c: &mut Criterion) {
 criterion_group!(
     benches,
     criterion_benchmark_curt_1,
-    criterion_benchmark_curt_5
+    criterion_benchmark_curt_5,
+    criterion_benchmark_curt_multi
 );
 criterion_main!(benches);
