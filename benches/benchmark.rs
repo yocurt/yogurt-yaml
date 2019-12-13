dc2fb5d77fd379d9eb80596b7ade50b8cc65099d
Johannes Jeising <Johannes.Jeising@gmail.com>
2019-12-13 21:45:36 +0100

    add benchmark check

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -19,6 +19,21 @@ fn criterion_benchmark_curt_multi(c: &mut Criterion) {
     });
 }
 
+fn criterion_benchmark_curt_multi_tags(c: &mut Criterion) {
+    let mut indicators = Vec::new();
+    let indicator_brackets = Indicators::new(&["ID", "ADD"], IdentRange::Brackets);
+    let indicator_closures = Indicators::new(&["REF", "END"], IdentRange::Closures);
+    let indicator_tags = Indicators::new(&["#", "@"], IdentRange::Tags);
+    indicators.push(indicator_brackets);
+    indicators.push(indicator_closures);
+    indicators.push(indicator_tags);
+    let mut curt = YogurtYaml::new(&indicators);
+    let test_data = "other @stuff ID[Test, \nTestContent: \"3\"] more\n REF{Test2, \nTestContent: [4]\n} #stu ADD[Test3, TestContent: [[a,7],[a,d]]]".repeat(5);
+    c.bench_function("YogurtYaml.curt(multi)", |b| {
+        b.iter(|| curt.curt(black_box(&test_data)))
+    });
+}
+
 fn criterion_benchmark_curt_5(c: &mut Criterion) {
     let mut curt = YogurtYaml::new_from_str(&["ID", "ADD", "REF", "END"]);
     let test_data = "other stuff ID[Test, \nTestContent: \"3\"] more\n REF[Test2, \nTestContent: [4]\n] stuADD[Test3, TestContent: [[a,7],[a,d]]]".repeat(5);
